28663f400c1605d2a895e227dc637c7477c8690b
Andrea D <andreadh01@gmail.com>
2023-09-05 23:40:03 -0700

    navbar responsiva, searchbar funcional y routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buscar/:texto', component: BuscarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
